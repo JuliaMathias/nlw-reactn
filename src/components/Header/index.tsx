223787b744a07feb6ea99b8255b64572117d2e58
JuliaMathias <juliamss@gmail.com>
2021-10-28 14:27:16 -0300

    feat: make button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 
 import { styles } from "./styles";
 
@@ -10,7 +10,9 @@ export function Header() {
     <View style={styles.container}>
       <LogoSvg />
 
-      <Text style={styles.logoutText}>Sair</Text>
+      <TouchableOpacity>
+        <Text style={styles.logoutText}>Sair</Text>
+      </TouchableOpacity>
     </View>
   );
 }
